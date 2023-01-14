2eef3a58dc7273f38a08c0da35377af279c9f1a6
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-14 23:27:18 +0600

    variabe added

--- game.js
+++ game.js
@@ -1,2 +1,8 @@
 const question = document.querySelector(".question");
 const choices = Array.from(document.querySelectorAll(".choice-text"));
+
+let currentQuestion = {};
+let acceptingAns = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestion = [];
