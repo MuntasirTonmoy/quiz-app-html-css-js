7f72115b2c22525728032f829c6b5bc2649b33f4
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-16 15:54:53 +0600

    button disabled based on blank name

--- end.js
+++ end.js
@@ -1,5 +1,20 @@
 const scoreElm = document.querySelector(".score");
 const score = localStorage.getItem("recentScore");
+const saveScoreForm = document.querySelector(".save-score-form");
 const userInput = document.querySelector(".user-input");
 const saveBtn = document.querySelector(".save-btn");
+
 scoreElm.innerText = `You score ${score}`;
+
+userInput.addEventListener("keyup", e => {
+  console.log(e.target.value);
+  if (e.target.value) {
+    saveBtn.removeAttribute("disabled");
+  } else {
+    saveBtn.setAttribute("disabled", 0);
+  }
+});
+saveScoreForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const userName = event.target.userName.value;
+});
